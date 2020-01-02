3f273e4f79d765c5baca0b1cb8eec99409539c37
Юрий Кузьминых <yurykuz65@gmail.com>
2020-01-02 14:35:36 +0300

    Сделал дом и поле боя Написал скрипт для дверей

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private Transform _rotationPivot;
+    [SerializeField] private bool _isLocked;
+    [SerializeField] private bool _clockwise;
+
+    private bool _isOpened = false;
+
+    public event Action DoorOpened;
+
+    public void Open()
+    {
+        if (_isLocked || _isOpened)
+            return;
+        float angle = _clockwise ? -90 : 90;
+        transform.RotateAround(_rotationPivot.position, Vector3.forward, angle);
+        _isOpened = true;
+        DoorOpened?.Invoke();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Open();
+    }
+}
