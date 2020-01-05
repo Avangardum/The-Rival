347648fea36de59f3cf1f0311733464ab4e1d475
Юрий Кузьминых <yurykuz65@gmail.com>
2020-01-05 12:35:43 +0300

    Исправил баг с зависанием игры после бектрекинга после смерти

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,7 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : SingletonMonoBehaviour<SceneLoader>
 {
-    public void LoadScene(string name) => SceneManager.LoadScene(name);
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+        Time.timeScale = 1;
+    }
 
-    public void ReloadScene() => LoadScene(SceneManager.GetActiveScene().name);
+    public void ReloadScene()
+    {
+        LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
 }
