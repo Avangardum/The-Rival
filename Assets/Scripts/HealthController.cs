bd839c80bffc421ee3897ca61df029a78e45b0ce
Юрий Кузьминых <yurykuz65@gmail.com>
2020-01-06 17:49:48 +0300

    Сделал рывок

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -7,19 +7,35 @@ public class HealthController : MonoBehaviour
     public int MaxHealth => _maxHealth;
     public float HealthPercentage => Health / MaxHealth;
     public bool IsDead => Health <= 0;
-    public bool IsInvincible;
 
     [SerializeField] private int _health;
     [SerializeField] private int _maxHealth;
+
+    private DashSkill _dashSkill;
+    private bool _isInvincible
+    {
+        get
+        {
+            if (_dashSkill != null)
+                if (_dashSkill.IsDashing)
+                    return true;
+            return false;
+        }
+    }
 
     public delegate void HealthChangedDelegate(int currentHealth, int maxHealth);
 
     public event HealthChangedDelegate HealthChanged;
     public event Action Death;
 
+    private void Awake()
+    {
+        _dashSkill = GetComponent<DashSkill>();
+    }
+
     public void Damage(int value)
     {
-        if (value <= 0 || IsInvincible)
+        if (value <= 0 || _isInvincible)
             return;
         ChangeHealth(-value);
     }
--- Assets/Scripts/PlayerSkills/DashSkill.cs
+++ Assets/Scripts/PlayerSkills/DashSkill.cs
@@ -6,8 +6,16 @@ public class DashSkill : MonoBehaviour
 {
     public bool IsEnabled;
 
+    public bool IsDashing { get; private set; } = false;
+
     [SerializeField] private float _distance;
     [SerializeField] private float _time;
+    [SerializeField] private float _cooldown;
+
+    private Vector2 _initialPosition;
+    private Vector2 _finalPosition;
+    private float _timePassed;
+    private float _currentCooldown = 0;
     
     private HealthController _healthController;
     private PlayerMovement _playerMovement;
@@ -20,7 +28,37 @@ public class DashSkill : MonoBehaviour
 
     public void Dash(Vector2 direction)
     {
-        if (!IsEnabled)
+        if (!IsEnabled || _currentCooldown > 0 || IsDashing)
             return;
+        IsDashing = true;
+        _timePassed = 0f;
+        _initialPosition = transform.position;
+        _finalPosition = (Vector2)transform.position + direction.normalized * _distance;
+        GetComponent<SpriteRenderer>().color = Color.red;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!IsDashing)
+            return;
+        _timePassed += Time.fixedDeltaTime;
+        float completion = _timePassed / _time;
+        if (completion > 1)
+            completion = 1;
+        transform.position = Vector2.Lerp(_initialPosition, _finalPosition, completion);
+        if(completion == 1)
+        {
+            IsDashing = false;
+            GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+        _currentCooldown = _cooldown;
+    }
+
+    private void Update()
+    {
+        _currentCooldown -= Time.deltaTime;
+        if (_currentCooldown < 0)
+            _currentCooldown = 0;
     }
 }
