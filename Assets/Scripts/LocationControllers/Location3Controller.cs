492f7dcb48fdaf9c75f6036490c400b1d88c9b0d
Юрий Кузьминых <yurykuz65@gmail.com>
2020-01-07 14:14:14 +0300

    Сделал 3 локацию

--- Assets/Scripts/LocationControllers/Location3Controller.cs
+++ Assets/Scripts/LocationControllers/Location3Controller.cs
@@ -6,6 +6,10 @@ public class Location3Controller : MonoBehaviour
 {
     [SerializeField] private string[] _introMonologue;
     [SerializeField] private string[] _endMonologue;
+    [SerializeField] private NewSkillNotification _dashNotification;
+    [SerializeField] private NewSkillNotification _fullCoveringNotification;
+    [SerializeField] private NewSkillNotification _levitationNotification;
+    [SerializeField] private NewSkillNotification _repulsionBombNotification;
 
     private void Start()
     {
@@ -15,6 +19,21 @@ public class Location3Controller : MonoBehaviour
 
     public void End()
     {
+        StartCoroutine(EndCoroutine());
+    }
+
+    private IEnumerator EndCoroutine()
+    {
         MonologueController.Instance.ShowMonologue(_endMonologue);
+        yield return new WaitWhile(() => MonologueController.Instance.IsMonologueActive);
+        NewSkillNotificationController.Instance.ShowNotification(_dashNotification);
+        yield return new WaitWhile(() => NewSkillNotificationController.Instance.IsNotificationActive);
+        NewSkillNotificationController.Instance.ShowNotification(_fullCoveringNotification);
+        yield return new WaitWhile(() => NewSkillNotificationController.Instance.IsNotificationActive);
+        NewSkillNotificationController.Instance.ShowNotification(_levitationNotification);
+        yield return new WaitWhile(() => NewSkillNotificationController.Instance.IsNotificationActive);
+        NewSkillNotificationController.Instance.ShowNotification(_repulsionBombNotification);
+        yield return new WaitWhile(() => NewSkillNotificationController.Instance.IsNotificationActive);
+        SceneLoader.Instance.LoadScene("Location 4");
     }
 }
